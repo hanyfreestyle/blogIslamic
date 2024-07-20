43be7a08e1f8e1ea32a4d7abc97e32a521762fab
Hany Darwish <hany.freestyle4u@gmail.com>
2024-07-20 19:48:53 +0300

    update

--- resources/views/components/admin/lang/meta-tage-seo.blade.php
+++ resources/views/components/admin/lang/meta-tage-seo.blade.php
@@ -6,15 +6,6 @@
 <div class="{{$desRow}}">
     <div class="row">
         <x-admin.form.trans-input name="name" :key="$key" :row="$row" :label="$defName" :tdir="$key"/>
-        @if($des)
-            <x-admin.form.trans-text-area name="des" :key="$key" :row="$row" :label="$defDes" :tdir="$key" add-class="bigTextArea"/>
-        @endif
-    </div>
-</div>
-
-
-<div class="{{$seoRow}}">
-    <div class="row">
         @if($slug)
             @if($viewtype == 'Add' )
                 <x-admin.form.trans-input name="slug" :key="$key" :row="$row" :label="__('admin/form.text_g_slug')" :tdir="$key"/>
@@ -26,7 +17,16 @@
                 @endcan
             @endif
         @endif
+        
+        @if($des)
+            <x-admin.form.trans-text-area name="des" :key="$key" :row="$row" :label="$defDes" :tdir="$key" add-class="bigTextArea"/>
+        @endif
+    </div>
+</div>
 
+
+<div class="{{$seoRow}}">
+    <div class="row">
         @if($seo)
             <x-admin.form.trans-input name="g_title" :key="$key" :row="$row" :label="__('admin/form.text_g_title')" :req="$seoReq" :tdir="$key"/>
             <x-admin.form.trans-text-area name="g_des" :key="$key" :row="$row" :label="__('admin/form.text_g_des')" :req="$seoReq" :tdir="$key"/>
