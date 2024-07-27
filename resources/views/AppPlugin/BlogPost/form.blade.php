93866afef8b8e0fef9c90aedd59f3418113a5040
Hany Darwish <hany.freestyle4u@gmail.com>
2024-07-27 14:15:51 +0300

    Update

--- resources/views/AppPlugin/BlogPost/form.blade.php
+++ resources/views/AppPlugin/BlogPost/form.blade.php
@@ -83,19 +83,19 @@
         //     return 'Any string';
         // };
 
-        function goodbye(e) {
-            if(!e) e = window.event;
-            //e.cancelBubble is supported by IE - this will kill the bubbling process.
-            e.cancelBubble = true;
-            e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
-
-            //e.stopPropagation works in Firefox.
-            if (e.stopPropagation) {
-                e.stopPropagation();
-                e.preventDefault();
-            }
-        }
-        window.onbeforeunload=goodbye;
+        // function goodbye(e) {
+        //     if(!e) e = window.event;
+        //     //e.cancelBubble is supported by IE - this will kill the bubbling process.
+        //     e.cancelBubble = true;
+        //     e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+        //
+        //     //e.stopPropagation works in Firefox.
+        //     if (e.stopPropagation) {
+        //         e.stopPropagation();
+        //         e.preventDefault();
+        //     }
+        // }
+        // window.onbeforeunload=goodbye;
 
     </script>
     <x-admin.table.sweet-delete-js/>
