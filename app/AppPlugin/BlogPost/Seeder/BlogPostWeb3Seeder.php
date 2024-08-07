7e14e7789edee5e9c66ff34850c5dc149d7bbfd3
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-07 15:15:31 +0300

    Update

--- app/AppPlugin/BlogPost/Seeder/BlogPostWeb3Seeder.php
+++ app/AppPlugin/BlogPost/Seeder/BlogPostWeb3Seeder.php
@@ -12,27 +12,7 @@ class BlogPostWeb3Seeder extends Seeder {
 
     public function run(): void {
 
-
-        set_time_limit(0);
-        ini_set('memory_limit', '20000M');
-
-
         BlogTranslation::unguard();
-        $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_6.sql');
-        DB::unprepared(file_get_contents($tablePath));
-
-        $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_7.sql');
-        DB::unprepared(file_get_contents($tablePath));
-
-        $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_8.sql');
-        DB::unprepared(file_get_contents($tablePath));
-
-        $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_9.sql');
-        DB::unprepared(file_get_contents($tablePath));
-
-        $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_10.sql');
-        DB::unprepared(file_get_contents($tablePath));
-
         $tablePath = public_path('db/SQLDumpSplitterResult/blog_translations_11.sql');
         DB::unprepared(file_get_contents($tablePath));
 
--- database/seeders/DatabaseWeb2Seeder.php
+++ database/seeders/DatabaseWeb2Seeder.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace Database\Seeders;
-use App\AppPlugin\BlogPost\Seeder\BlogPostWeb2Seeder;
+ 
+use App\AppPlugin\BlogPost\Seeder\BlogPostWeb3Seeder;
 use Illuminate\Database\Seeder;
 
 
 class DatabaseWeb2Seeder extends Seeder {
 
     public function run(): void {
-        $this->call(BlogPostWeb2Seeder::class);
+        $this->call(BlogPostWeb3Seeder::class);
 
     }
 }
