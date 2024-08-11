af84c257472dee29b4ac1cbfd22d1210316453f2
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-11 13:43:37 +0300

    Update

--- app/Helpers/TableOfContents/HeadersParser.php
+++ app/Helpers/TableOfContents/HeadersParser.php
@@ -2,20 +2,17 @@
 
 namespace App\Helpers\TableOfContents;
 
-final class HeadersParser
-{
+final class HeadersParser {
     private $tags;
     private $text;
     private $parsedHeaders = [];
 
-    public function __construct(array $tags, string $text)
-    {
+    public function __construct(array $tags, string $text) {
         $this->tags = $tags;
         $this->text = $text;
     }
 
-    public function getParsedHeaders()
-    {
+    public function getParsedHeaders() {
         $patternString = '(' . implode('|', $this->tags) . ')';
         $pattern = "~<{$patternString}([^>]*)>(.*)</\s*\g1\s*>~i";
         preg_replace_callback($pattern, [$this, 'handleMatchedTag'], $this->text);
@@ -30,8 +27,7 @@ final class HeadersParser
      * 'level 1' etc.
      * @param $matchedTag
      */
-    private function handleMatchedTag($matchedTag)
-    {
+    private function handleMatchedTag($matchedTag) {
         $header = [];
         if (trim($matchedTag[3])) {
             $header['level'] = array_search($matchedTag[1], $this->tags);
@@ -41,22 +37,20 @@ final class HeadersParser
         return;
     }
 
-    private function validateHeadersStructure() : void
-    {
+    private function validateHeadersStructure(): void {
         foreach ($this->parsedHeaders as $key => $header) {
             $this->deleteUnstructuredHeader($key, $this->parsedHeaders);
         }
     }
 
-    private function deleteUnstructuredHeader(int $key, &$headers) : void
-    {
-        if ($key > 0 && ($headers[$key]['level'] - $headers[$key - 1]['level'] > 1)) {
-            unset($headers[$key]);
-        }
+    private function deleteUnstructuredHeader(int $key, &$headers): void {
+//dd($headers);
+//        if ($key > 0 && ($headers[$key]['level'] - $headers[$key - 1]['level'] > 1)) {
+//            unset($headers[$key]);
+//        }
     }
 
-    private function addHeaderId()
-    {
+    private function addHeaderId() {
         foreach ($this->parsedHeaders as $key => &$header) {
             $header['id'] = $key + 1;
         }
