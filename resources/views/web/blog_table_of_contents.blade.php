13783cd81b359f8c470324ce727c2a03312923c1
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-11 12:58:35 +0300

    Update

--- resources/views/web/blog_table_of_contents.blade.php
+++ resources/views/web/blog_table_of_contents.blade.php
@@ -1,7 +1,7 @@
 <ul>
     @foreach($contents as $key => $header)
         @if(issetArr($header,'id'))
-            <li><a href="#{{$header['id']}}-{{\App\Helpers\AdminHelper::Url_Slug(strip_tags($header['header']))}}">{{strip_tags($header['header'])}}</a></li>
+            <li><a href="#{{$header['id']}}-{{\App\Helpers\AdminHelper::Url_Slug(strip_tags($header['header']))}}">{!! strip_tags($header['header']) !!}</a></li>
         @endif
         @if (!empty($header['childs']))
             @include('web.blog_table_of_contents', ['contents' => $header['childs']])
